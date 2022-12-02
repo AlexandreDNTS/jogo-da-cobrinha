c09abd7a845e5f2b80ca9a109d0c2c954c8f4be3
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2022-12-02 13:13:57 -0300

    mensagem de Game over

--- main.py
+++ main.py
@@ -46,7 +46,23 @@ def aumenta_cobra(lista_cobra):
         # XeY[0] = x
         # XeY[1] = y
         pygame.draw.rect(tela, (0, 255, 0), (XeY[0], XeY[1], 20, 20))
-# loop infinito para roda o jogo
+
+
+morreu = False
+
+
+def reinicia_jogo():
+    global morreu, pontos, comprimento_inicial, y_controle, x_controle, y_maca, x_maca, y_snake, x_snake, lista_cabeca, lista_cobra
+    morreu = False
+    pontos = 0
+    comprimento_inicial = 2
+    y_maca = altura//2
+    x_maca = 580
+    y_snake = altura//2
+    x_snake = largura//2
+    lista_cabeca = []
+    lista_cobra = []
+    # loop infinito para roda o jogo
 
 
 while True:
@@ -64,31 +80,31 @@ while True:
             pygame.quit()
             exit()
     # movimentação
-    if event.type == KEYDOWN:
-        if event.key == K_a:
-            if x_controle == velocidade:
-                pass
-            else:
-                x_controle = -velocidade
-                y_controle = 0
-        if event.key == K_d:
-            if x_controle == -velocidade:
-                pass
-            else:
-                x_controle = velocidade
-                y_controle = 0
-        if event.key == K_w:
-            if y_controle == velocidade:
-                pass
-            else:
-                y_controle = -velocidade
-                x_controle = 0
-        if event.key == K_s:
-            if y_controle == -velocidade:
-                pass
-            else:
-                y_controle = velocidade
-                x_controle = 0
+        if event.type == KEYDOWN:
+            if event.key == K_a:
+                if x_controle == velocidade:
+                    pass
+                else:
+                    x_controle = -velocidade
+                    y_controle = 0
+            if event.key == K_d:
+                if x_controle == -velocidade:
+                    pass
+                else:
+                    x_controle = velocidade
+                    y_controle = 0
+            if event.key == K_w:
+                if y_controle == velocidade:
+                    pass
+                else:
+                    y_controle = -velocidade
+                    x_controle = 0
+            if event.key == K_s:
+                if y_controle == -velocidade:
+                    pass
+                else:
+                    y_controle = velocidade
+                    x_controle = 0
     x_snake = x_snake + x_controle
     y_snake = y_snake + y_controle
     # criando a maçã e a cobra
@@ -106,6 +122,22 @@ while True:
     lista_cabeca.append(x_snake)
     lista_cabeca.append(y_snake)
     lista_cobra.append(lista_cabeca)
+    if lista_cobra.count(lista_cabeca) > 1:
+        fonte2 = pygame.font.SysFont('arial', 20, True, True)
+        msg2 = 'Game over! pressione a tecla R para jogar novamente'
+        textoFormatado2 = fonte2.render(msg2, True, (0, 0, 0))
+        morreu = True
+        while morreu:
+            tela.fill((255, 255, 255))
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_r:
+                        reinicia_jogo()
+            tela.blit(textoFormatado2, (55, altura//2))
+            pygame.display.update()
     if len(lista_cobra) > comprimento_inicial:
         del lista_cobra[0]
     aumenta_cobra(lista_cobra)
